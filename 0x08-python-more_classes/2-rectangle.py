54ae6bec0f781d65e8036412799893cd6ea88865
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-06-28 00:32:54 +0100

    Create 2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+
+        self.width = width
+
+        self.height = height
+
+    
+
+    @property
+
+    def width(self):
+
+        return self.__width
+
+    @width.setter
+
+    def width(self, val):
+
+        if type(val) != int:
+
+            raise TypeError('width must be an integer')
+
+        elif val < 0:
+
+            raise ValueError('width must be >= 0')
+
+        self.__width = val
+
+    @property
+
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+
+    def height(self, val):
+
+        if type(val) != int:
+
+            raise TypeError('height must be an integer')
+
+        elif val < 0:
+
+            raise ValueError('height must be >= 0')
+    def area(self):
+
+            return self.__width * self.__height
+    def perimeter(self):
+
+        if self.__width == 0 or self.__height == 0:
+
+            return 0
+
+        return (2 * self.__width) + (2 * self.__height)
+
+    
