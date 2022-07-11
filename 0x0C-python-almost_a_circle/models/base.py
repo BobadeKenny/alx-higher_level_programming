9ca8b70cdec3c8b20fae573b9e7ab52681fabb12
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-07-11 07:03:28 +0100

    checkpoint 19 mandatory done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ This class will be the “base” of all other classes in this project
 """
 
 
+import json
+
+
 class Base:
     """Base class
     """
@@ -20,3 +23,51 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return ("[]")
+        return(json.dumps(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON
+        string representation json_string
+        """
+        if json_string is None:
+            return []
+        return(json.loads(json_string))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+        """
+        if list_objs is None:
+            dicts = []
+        else:
+            dicts = [i.to_dictionary() for i in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(Base.to_json_string(dicts))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set
+        """
+        new_class = cls(1, 1)
+        new_class.update(**dictionary)
+        return(new_class)
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances
+        """
+        filename = cls.__name__ + ".json"
+        instances = []
+        with open(filename, encoding="utf-8") as f:
+            json_string = f.read()
+            instances = [cls.create(**obj) for obj in cls.from_json_string(json_string)]
+        return(instances)
