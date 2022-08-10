b483477ba419fb8468b09143d05b4dc6df5075a8
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-08-11 00:13:04 +0100

    corrections

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 --  lists all genres from hbtn_0d_tvshows
 SELECT name, count(*) AS number_of_shows FROM tv_genres 
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY name ORDER BY number_of_shows DESC;
+GROUP BY genre_id ORDER BY number_of_shows DESC;
