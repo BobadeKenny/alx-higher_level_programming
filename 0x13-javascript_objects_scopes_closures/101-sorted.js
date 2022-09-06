14eb98b927ce7de7d0da2c112f9c0e1286eb2ea3
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-09-06 22:50:18 +0100

    advanced

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const sorted = {};
+
+Object.keys(dict).forEach(key => {
+  if (!sorted[dict[key]]) {
+    sorted[dict[key]] = [];
+  }
+  sorted[dict[key]].push(key);
+}
+);
+console.log(sorted);
