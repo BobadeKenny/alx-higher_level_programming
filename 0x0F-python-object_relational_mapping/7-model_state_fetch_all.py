d288a70c98b29dbbdea9a93d924774b8a89033c4
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-09-09 18:56:12 +0100

    style fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,8 +6,8 @@ from sys import argv
 from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
-                        .format(argv[1], argv[2], argv[3]),
-                        encoding='latin1')
+                           .format(argv[1], argv[2], argv[3]),
+                           encoding='latin1')
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
