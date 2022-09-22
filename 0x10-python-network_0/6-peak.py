075df964cbdc7fde6946498c3e4f4c8c891dae76
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-09-22 22:31:03 +0100

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" function find_peak """
+
+def get_peak(arr, low, high, n):
+    '''get peak'''
+    mid = (low + high) // 2
+    if ((mid == 0 or arr[mid - 1] <= arr[mid])
+    and (mid == n-1 or arr[mid + 1] <+ arr[mid])):
+        return arr[mid]
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return (get_peak(arr, low, mid - 1, n))
+    else:
+        return (get_peak(arr, mid + 1, high, n))
+def find_peak(list_of_integers):
+    '''find peak integer'''
+    if (list_of_integers):
+        n = len(list_of_integers)
+        return get_peak(list_of_integers, 0, n - 1, n)
+    return None
